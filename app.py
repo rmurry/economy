430ca80b31a395dcd1963cecce0fc9d8fc905fde
Rylan <rylanmurry@gmail.com>
2024-08-25 20:08:20 -0500

    implemented an altair chart

--- app.py
+++ app.py
@@ -5,27 +5,35 @@ from datetime import timedelta
 import yfinance as yf
 import easychart
 import pandas as pd
+import altair as alt
 
 st.set_page_config(page_title="Stonks")
 
-st.title("Stock Puller")
 
-st.sidebar.markdown("Enter a Stock Symbol and Date Range to Pull Data For")
+with st.sidebar:
+    st.title("Stock Puller")
 
-symbol = st.sidebar.text_input(label = "Stock Symbol",value="AAPL")
+    st.markdown("Enter a Stock Symbol and Date Range to Pull Data For")
 
-start_date = st.sidebar.date_input(label = "Starting Date",value = date.today()-timedelta(weeks = 12))
-end_date = st.sidebar.date_input(label = "Ending Date",value = date.today())
+    symbol = st.text_input(label = "Stock Symbol",value="AAPL")
+
+    start_date = st.date_input(label = "Starting Date",value = date.today()-timedelta(weeks = 12))
+    end_date = st.date_input(label = "Ending Date",value = date.today())
 
 def get_data(symbol,start_date,end_date):
-    yf.download(symbol,start=start_date,end=end_date)
+    results = pd.DataFrame(yf.download(symbol,start=start_date,end=end_date)).reset_index()
+    return results
 
-#dat = yf.download("AAPL",start="2024-01-01",end="2024-03-01")
-#dat.head()
 dat = get_data(symbol,start_date,end_date)
 
-trend = easychart.new("line")
-trend.plot(dat)
+#dat = get_data("AAPL","2024-01-01","2024-05-01")
+#dat.head()
 
-st.components.v1.html(easychart.rendering.render(trend), height=400)
+trend = alt.Chart(dat).mark_line().encode(
+    x = 'Date',
+    y = 'Close'
+)
+st.altair_chart(trend,use_container_width=True)
+
+
 
