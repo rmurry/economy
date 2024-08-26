ce227f52317aad0de6cfdd0da92c20849d5468ea
Rylan <rylanmurry@gmail.com>
2024-08-25 19:22:09 -0500

    converted the obtaining of data to a function

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import yfinance as yf
 import easychart
 import pandas as pd
 
-easychart.config.rendering.responsive = True
+st.set_page_config(page_title="Stonks")
+
 st.title("Stock Puller")
 
 st.sidebar.markdown("Enter a Stock Symbol and Date Range to Pull Data For")
@@ -14,11 +15,14 @@ st.sidebar.markdown("Enter a Stock Symbol and Date Range to Pull Data For")
 symbol = st.sidebar.text_input(label = "Stock Symbol",value="AAPL")
 
 start_date = st.sidebar.date_input(label = "Starting Date",value = date.today()-timedelta(weeks = 12))
-end_date = st.sidebar.date_input(label = "Ending Date")
+end_date = st.sidebar.date_input(label = "Ending Date",value = date.today())
 
-dat = yf.download(symbol,start=start_date,end=end_date)
+def get_data(symbol,start_date,end_date):
+    yf.download(symbol,start=start_date,end=end_date)
+
 #dat = yf.download("AAPL",start="2024-01-01",end="2024-03-01")
 #dat.head()
+dat = get_data(symbol,start_date,end_date)
 
 trend = easychart.new("line")
 trend.plot(dat)
